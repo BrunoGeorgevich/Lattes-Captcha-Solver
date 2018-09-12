a84605e0624e3aa258693f9386cb2e7bd48e1988
BrunoGeorgevich <brunogeorgevich@hotmail.com>
2018-09-12 20:37:37 -0300

    Method 2 with Skeletonize - 90.25%

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import os
 import cv2
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from skimage.morphology import skeletonize
 
-def skeletonize(img):
-    size = np.size(img)
-    skeleton_img = np.zeros(img.shape, np.uint8)
+template_dir = "Best/"
+captchas_dir = "Captchas/"
 
-    ret, img = cv2.threshold(img, 127, 255, 0)
-    element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-    done = False
 
-    while not done:
-        eroded = cv2.erode(img, element)
-        temp = cv2.dilate(eroded, element)
-        temp = cv2.subtract(img, temp)
-        skeleton_img = cv2.bitwise_or(skeleton_img, temp)
-        img = eroded.copy()
-
-        zeros = size - cv2.countNonZero(img)
-        if zeros == size:
-            done = True
-
-    return skeleton_img
+# def skeletonize(img):
+#     size = np.size(img)
+#     skeleton_img = np.zeros(img.shape, np.uint8)
+#
+#     ret, img = cv2.threshold(img, 127, 255, 0)
+#     element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+#     done = False
+#
+#     while not done:
+#         eroded = cv2.erode(img, element)
+#         temp = cv2.dilate(eroded, element)
+#         temp = cv2.subtract(img, temp)
+#         skeleton_img = cv2.bitwise_or(skeleton_img, temp)
+#         img = eroded.copy()
+#
+#         zeros = size - cv2.countNonZero(img)
+#         if zeros == size:
+#             done = True
+#
+#     return skeleton_img
 
 
 def reconstruct(seeds, mask):
@@ -38,6 +45,7 @@ def reconstruct(seeds, mask):
 
     return seeds
 
+
 def color_thresholding(image, min_val_hsv, max_val_hsv):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, min_val_hsv, max_val_hsv)
@@ -46,18 +54,13 @@ def color_thresholding(image, min_val_hsv, max_val_hsv):
     extracted[imask] = image[imask]
     return extracted
 
+
 def process_captcha(file_path):
     captcha = cv2.imread(file_path, cv2.IMREAD_COLOR)
     gray = cv2.cvtColor(captcha, cv2.COLOR_BGR2GRAY)
 
-    only_text = color_thresholding(captcha, (0, 0, 225), (115, 73, 255))
-    # only_text = color_thresholding(captcha, (0, 0, 255), (0, 0, 255))
-
     filter = np.ones(gray.shape, dtype=np.uint8)
     filter[:, int(filter.shape[1] * 0.60):] *= 0
-
-    # only_text = cv2.bitwise_or(cv2.bitwise_or(only_text[:, :, 0], only_text[:, :, 1]), only_text[:, :, 0])
-    # only_text = cv2.bitwise_and(only_text, filter)
 
     _, foreground = cv2.threshold(gray, 238, 255, cv2.THRESH_BINARY)
 
@@ -66,15 +69,7 @@ def process_captcha(file_path):
 
     cv2.normalize(foreground, foreground, 255, 0, cv2.NORM_MINMAX)
 
-    # cv2.imshow("Image", foreground)
-    #
-    # while cv2.waitKey(0) != ord('q'):
-    #     pass
-
     return foreground
-
-template_dir = "Best/"
-captchas_dir = "Captchas/"
 
 
 def has_someone(chosen_chars, min, max):
@@ -93,50 +88,153 @@ def has_someone(chosen_chars, min, max):
 def at_least_distance(chosen_chars, new_char):
     index = 0
     for char in chosen_chars:
-        if len(np.intersect1d(range(char[2][0], char[2][0] + char[3][0]), range(new_char[2][0], new_char[2][0] + new_char[3][0]))):
+        if len(np.intersect1d(range(char[2][0], char[2][0] + char[3][0]),
+                              range(new_char[2][0], new_char[2][0] + new_char[3][0]))):
             return index
         index += 1
     return -1
 
 
-words = []
-for i in range(605):
-    img = process_captcha(captchas_dir + str(i) + ".png")
-    templates_files = os.listdir(template_dir)
-    probabilities = []
+def method_1():
+    words = []
+    for i in range(605):
+        img = process_captcha(captchas_dir + str(i) + ".png")
+        templates_files = os.listdir(template_dir)
+        probabilities = []
 
-    for template_path in templates_files:
-        template_name = template_path.replace(".JPG", "")
-        template = cv2.imread(template_dir + template_path, cv2.IMREAD_GRAYSCALE)
+        for template_path in templates_files:
+            template_name = template_path.replace(".JPG", "")
+            template = cv2.imread(template_dir + template_path, cv2.IMREAD_GRAYSCALE)
 
-        w, h = template.shape[::-1]
-        res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-        probabilities.append((template_name, max_val, (max_loc[0], max_loc[1]), (w, h)))
+            w, h = template.shape[::-1]
+            res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            probabilities.append((template_name, max_val, (max_loc[0], max_loc[1]), (w, h)))
 
-    probabilities.sort(key=lambda tup: tup[1], reverse=True)
-    chosen_chars = []
+        probabilities.sort(key=lambda tup: tup[1], reverse=True)
+        chosen_chars = []
 
-    for p in probabilities:
-        if len(chosen_chars) == 0:
-            chosen_chars.append(p)
-        elif at_least_distance(chosen_chars, p):
-            index = at_least_distance(chosen_chars, p)
-            if index == -1 and len(chosen_chars) < 4:
+        for p in probabilities:
+            if len(chosen_chars) == 0:
                 chosen_chars.append(p)
-            elif chosen_chars[index][1] < p[1]:
-                chosen_chars[index] = p
+            elif at_least_distance(chosen_chars, p):
+                index = at_least_distance(chosen_chars, p)
+                if index == -1 and len(chosen_chars) < 4:
+                    chosen_chars.append(p)
+                elif chosen_chars[index][1] < p[1]:
+                    chosen_chars[index] = p
 
-    chosen_chars.sort(key=lambda tup: tup[2][0])
+        chosen_chars.sort(key=lambda tup: tup[2][0])
 
+        word = ""
+        for char in chosen_chars:
+            word += char[0]
+
+        print(word)
+        words.append(word)
+        print("%.2f %%" % (100 * (i / 604)))
+
+    file = open("Method_1.txt", 'w')
+    for word in words:
+        file.write("%s\n" % word)
+
+
+def to_text(best):
     word = ""
-    for char in chosen_chars:
-        word += char[0]
+    word += best[0][0]
+    word += best[1][0]
+    word += best[2][0]
+    word += best[3][0]
+    return word
 
-    print(word)
-    words.append(word)
-    print("%.2f %%" % (100 * (i / 604)))
+def method_2():
 
-file = open("teste.txt", 'w')
-for word in words:
-    file.write("%s\n" % word)
+    template_max_pixels = pd.read_csv("max_pixels_templates.csv", sep=',')
+    template_max_pixels = template_max_pixels.set_index("Template", drop=False)
+
+    words = []
+    for i in range(605):
+        img = process_captcha(captchas_dir + str(i) + ".png")
+
+        cv2.normalize(img, img, 1, 0, cv2.NORM_MINMAX)
+        img = np.float32(img)
+
+        templates_files = os.listdir(template_dir)
+        probabilities = []
+
+        for template_path in templates_files:
+            template_name = template_path.replace(".JPG", "")
+            template = cv2.imread(template_dir + template_path, cv2.IMREAD_GRAYSCALE)
+            _, thresh = cv2.threshold(template, 80, 255, cv2.THRESH_BINARY)
+
+            cv2.normalize(thresh, thresh, 1, 0, cv2.NORM_MINMAX)
+            template = np.float32(thresh)
+
+            template = skeletonize(np.array(template, dtype=np.float32))
+            template = np.array(template, dtype=np.float32)
+
+            w, h = template.shape[::-1]
+            res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+            probabilities.append((template_name, max_val/template_max_pixels.loc[template_name, "Max Pixels"], (max_loc[0], max_loc[1]), (w, h)))
+
+        probabilities.sort(key=lambda tup: tup[1], reverse=True)
+        best = probabilities[0:4]
+        best.sort(key=lambda tup: tup[2][0], reverse=False)
+
+        word = to_text(best)
+        print(word)
+        words.append(word)
+        print("%.2f %%" % (100 * (i / 604)))
+
+    file = open("Method_2.txt", 'w')
+    correct = 0
+
+    feedback_file = open("first_letter.txt", 'r')
+    feedback_data = feedback_file.read()
+    feedback_file.close()
+
+    feedback_data = feedback_data.split(sep='\n')
+
+    index = 0
+    for word in words:
+        if word == feedback_data[index]:
+            correct += 1
+        file.write("%s\n" % word)
+        index += 1
+
+    print("Acerto:", str(float(correct/605)))
+
+
+
+# templates_files = os.listdir(template_dir)
+#
+# print("Template,Max Pixels")
+# for template_path in templates_files:
+#     template_name = template_path.replace(".JPG", "")
+#     img = cv2.imread(template_dir + template_path, cv2.IMREAD_GRAYSCALE)
+#     _, thresh = cv2.threshold(img, 80, 255, cv2.THRESH_BINARY)
+#
+#     cv2.normalize(thresh, thresh, 1, 0, cv2.NORM_MINMAX)
+#     thresh = np.float32(thresh)
+#
+#     skel = skeletonize(np.array(thresh, dtype=np.float32))
+#     skel = np.array(skel, dtype=np.float32)
+#
+#     res = cv2.matchTemplate(thresh, skel, cv2.TM_CCOEFF_NORMED)[0][0]
+#     print(template_name + "," + str(res))
+
+# template_max_pixels = pd.read_csv("max_pixels_templates.csv", sep=',')
+# template_max_pixels = template_max_pixels.set_index("Template", drop = False)
+#
+# print(template_max_pixels.loc["1", "Max Pixels"])
+
+
+method_2()
+#
+# feedback_file = open("first_letter.txt", 'r')
+# feedback_data = feedback_file.read()
+# feedback_file.close()
+#
+# print(feedback_data.split(sep='\n')[0])
